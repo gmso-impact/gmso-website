91637a9e988c32b9b0cfe6f7cbcaed8a93bbac45
BBudnicki <brandon@Budnicki.net>
2022-01-07 22:57:48 -0600

    show stories with images

--- src/components/storyList/storyItem.vue
+++ src/components/storyList/storyItem.vue
@@ -1,7 +1,9 @@
 <template lang="">
-  <div class="col-md-12">
-    <div class="card mb-3">
-      <img class="card-img-top" alt="Card image cap" />
+  <div class="col-md-6" v-if="story.fields['Story Title'] && story.fields['Story Card Image']">
+    <div class="card mb-3 mx-2">
+      <img v-if="story.fields['Story Card Image']"
+       :src="story.fields['Story Card Image'][0].thumbnails.large.url"
+       class="card-img-top" alt="Card image cap" />
       <div class="card-body">
         <h5 class="card-title">{{story.fields['Story Title']}}</h5>
         <p class="card-text">
