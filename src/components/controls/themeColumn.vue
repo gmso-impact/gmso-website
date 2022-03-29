013d3f5152e46917fa6c9fb24c4a0317ecab45da
CitSci.org <webmaster@citsci.org>
2022-03-29 13:08:07 -0600

    added-theme-icon

--- src/components/controls/themeColumn.vue
+++ src/components/controls/themeColumn.vue
@@ -5,11 +5,11 @@
       <label class="btn btn-white" for="btnradio2">Español</label>
     </div>
 
-    <button class="btn btn-white mt-2 mt-xxl-4 Greg" v-on:click="resetTagsClicked">
+    <button class="btn btn-white mt-2 mt-xxl-4" v-on:click="resetTagsClicked">
       All
     </button>
 
-    <ThemeButton class="Greg"
+    <ThemeButton
       v-for="storyTheme in storyThemes"
       :key="storyTheme.name"
       v-bind:control="storyTheme"
@@ -46,15 +46,5 @@ export default {
 </script>
 
 <style>
-
-.Greg{
-  background-image:url('https://ibis-test1.nrel.colostate.edu/GMSO/images/icons/Food.png');
-  background-size: auto 90%;
-  background-repeat: no-repeat;
-  background-attachment: fixed;
-  background-position: left center;
-  height: 100%;
-  border-radius: 0px !important;
-}
 
 </style>
