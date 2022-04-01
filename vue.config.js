1598329fed4d9cc07c34008502f44090c179363c
BBudnicki <brandon@Budnicki.net>
2022-04-01 12:01:07 -0600

    Update Production file paths

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 // vue.config.js
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/gmso-website/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/" : "/",
 };
