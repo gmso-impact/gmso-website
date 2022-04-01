fe56700a9f7e0afe3cd7f76c0a8ceff96c188f4a
BBudnicki <brandon@Budnicki.net>
2022-04-01 13:29:13 -0600

    Add kiosk switch

--- src/views/Kiosk.vue
+++ src/views/Kiosk.vue
@@ -1,15 +1,25 @@
 <template>
-  <Main></Main>
+  <Main
+      v-on:click="setLastInteraction"
+  ></Main>
 </template>
 
 <script lang="ts">
 // @ is an alias to /src
 import Main from "@/components/main/Main.vue";
+import { mapActions, mapMutations } from "vuex";
 
 export default {
   name: "Home",
   components: {
     Main,
+  },
+    mounted() {
+    this.startTimer();
+  },
+  methods: {
+    ...mapActions({ startTimer: "startTimer" }),
+    ...mapMutations({ setLastInteraction: "setLastInteraction" }),
   },
 };
 </script>
