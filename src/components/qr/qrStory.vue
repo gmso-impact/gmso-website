5e4eecedf801d7bfdb0dd63ff64cc2bd9408898d
BBudnicki <brandon@Budnicki.net>
2022-04-15 20:35:38 -0600

    fix geo alignment in web mode

--- src/components/qr/qrStory.vue
+++ src/components/qr/qrStory.vue
@@ -1,10 +1,14 @@
 <template lang="">
-  <div class="qrStory" v-if="this.$route.name === 'Kiosk'">
+  <div class="qrStory">
+  <div v-if="this.$route.name === 'Kiosk'">
+
+
     <div class="text-center text-white small pb-1">open on your phone</div>
     <button class="btn btn-block btn-secondary d-none" v-on:click="printURL()">
       printURL
     </button>
     <vue-qr class="w-100" :text="url" :size="1200" qid="testid"></vue-qr>
+  </div>
   </div>
 </template>
 <script>
