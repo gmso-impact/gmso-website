128add327bb3189a14f4b3786990a6042df39ee8
BBudnicki <brandon@Budnicki.net>
2022-04-16 16:14:31 -0600

    smooth story list animation

--- src/components/storyList/storyList.vue
+++ src/components/storyList/storyList.vue
@@ -3,29 +3,21 @@
     <h3 class="text-center my-1 my-xxl-5 bg-white d-xl-none">
       {{ $t(`tagline`) }}
     </h3>
-    <div class="flex-grow-1 overflow-y-auto overflow-x-hidden">
-      <div class="p-2 carbon min-100 d-flex flex-column justify-content-center">
-        <transition-group name="list-complete"  tag="div" class="row no-gutters">
-          <StoryItem
-            v-for="story in stories"
-            :key="story.id"
-            class="list-complete-item"
-            v-bind:story="story"
-          >
-          </StoryItem>
-        </transition-group>
-        <div class="row justify-content-md-center">
-          <div
-            v-if="stories.length < 3"
-            class="col-xxl-4 pb-5 mb-5 pt-3 text-white"
-          >
-            <!--<h3>
-              No more stories appear in the map. Please zoom out
-              or <button class="btn btn-primary btn-lg">Reset</button>
-            </h3>-->
-          </div>
-        </div>
-      </div>
+    <div class="flex-grow-1 overflow-y-auto overflow-x-hidden carbon p-2">
+      <transition-group
+        name="story-list"
+        tag="div"
+        class="story-list d-flex h-100 align-content-center row no-gutters"
+        @before-leave="beforeLeave"
+      >
+        <StoryItem
+          v-for="story in stories"
+          :key="story.id"
+          class="story"
+          v-bind:story="story"
+        >
+        </StoryItem>
+      </transition-group>
     </div>
   </div>
 </template>
@@ -46,18 +38,59 @@ export default {
       stories: "storyInMap",
     }),
   },
+  methods: {
+    beforeLeave(el) {
+      const { marginLeft, marginTop, width, height } =
+        window.getComputedStyle(el);
+      el.style.left = `${el.offsetLeft - parseFloat(marginLeft, 10)}px`;
+      el.style.top = `${el.offsetTop - parseFloat(marginTop, 10)}px`;
+      el.style.width = width;
+      el.style.height = height;
+    },
+  },
 };
 </script>
 <style lang="scss">
-.list-complete-item {
-  transition: all 1s;
-}
-.list-complete-enter, .list-complete-leave-to
-/* .list-complete-leave-active below version 2.1.8 */ {
-  opacity: 0;
-  transform: translateX(-10px);
-}
-.list-complete-leave-active {
+// enter = new element apears
+// leave = existing element disapears
+// move = element moves to a new position
 
+// null = begining state of animation
+// to = end state of animation
+// active = duration of animation = null + to
+.story-list {
+  position: relative;
+}
+.story {
+  backface-visibility: hidden;
+  z-index: 1;
+  display: inline-block;
+}
+
+/* moving */
+.story-list-move {
+  transition: all 500ms ease;
+}
+.story-list-move-active {
+}
+
+/* appearing */
+.story-list-enter {
+  opacity: 0;
+  transform: scale(0.8, 0.8);
+}
+.story-list-enter-active {
+  transition: all 600ms ease-out;
+}
+
+/* disappearing */
+.story-list-leave-active {
+  transition: all 1000ms ease-in;
+  z-index: 0;
+  position: absolute;
+}
+.story-list-leave-to {
+  opacity: 0;
+  transform: scale(0.8, 0.8);
 }
 </style>
