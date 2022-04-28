e0a8e980b98db390830f8811d4ea34cfd54c3d6f
BBudnicki <brandon@Budnicki.net>
2022-04-28 13:35:10 -0600

    theme buttons icon added

--- src/components/controls/themeButton.vue
+++ src/components/controls/themeButton.vue
@@ -1,14 +1,31 @@
 <template lang="">
-<div class="col-xxl-12">
-  <button
-    class="btn-xxl-lg btn-block mt-2 themeIcon font-weight-bold px-0 py-0 carbon text-white"
-    :key="control.name"
-    v-on:click="setTagClicked"
-  >
-    <!--<img v-if='control.name === "Food"' class='float-left theme-icon' src='Food.png' />-->
-    <div :class="buttonClasses" class="float-left h-100 xxl-buttonWallSize">&nbsp;</div>
-    <span class="float-left ml-xxl-3">{{ $t(`storyThemes.${control.name}`) }}</span>
-  </button>
+  <div class="col-xxl-12">
+    <div
+      class="themeButton btn-dark d-flex flex-row mb-2 mb-xxl-4"
+      :class="themeButtonClasses"
+      :key="control.name"
+      v-on:click="setTagClicked"
+    >
+      <div class="themeIconContainer py-2 py-xxl-4 px--xxl-3" :class="[`bg-${control.name}`]">
+        <img :src="themeIconSRC" class="themeIcon" />
+      </div>
+      <div class="themeText flex-fill text-center p-2">
+        {{ $t(`storyThemes.${control.name}`) }}
+      </div>
+    </div>
+
+    <!--<button
+      class="btn-xxl-lg btn-block mt-2 themeIcon font-weight-bold px-0 py-0 carbon text-white"
+      :key="control.name"
+      v-on:click="setTagClicked"
+    >
+      <div :class="buttonClasses" class="float-left h-100 xxl-buttonWallSize">
+        &nbsp;
+      </div>
+      <span class="float-left ml-xxl-3">{{
+        $t(`storyThemes.${control.name}`)
+      }}</span>
+    </button>-->
   </div>
 </template>
 <script>
@@ -26,6 +43,18 @@ export default {
     },
   },
   computed: {
+    themeButtonClasses: function () {
+      // `this` points to the vm instance
+      const buttonStyle = this.control.isActive
+        ? `bg-${this.control.name}`
+        : "";
+
+      // add theme icons
+      const icon = `theme-${this.control.name}`;
+
+      return [buttonStyle, icon];
+    },
+
     buttonClasses: function () {
       // `this` points to the vm instance
       const buttonStyle = this.control.isActive
@@ -36,6 +65,9 @@ export default {
       const icon = `theme-${this.control.name}`;
 
       return [buttonStyle, icon];
+    },
+    themeIconSRC: function () {
+      return `theme/${this.control.name}_IMpact_white.svg`;
     },
   },
   methods: {
@@ -53,14 +85,17 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
-
 .theme-icon-xxl {
   height: 24px;
 }
+
+.themeIconContainer {
+  width: 10%;
+}
+
 
 .buttonWallSize {
   min-height: 160px !important;
   min-width: 110px !important;
 }
-
 </style>
