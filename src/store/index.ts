6339d491b8bd5c0a39da1c4147d9b8ad0ea0e5e1
BBudnicki <brandon@Budnicki.net>
2022-09-27 15:52:48 -0600

    Disable video story reset

--- src/store/index.ts
+++ src/store/index.ts
@@ -14,7 +14,7 @@ export default new Vuex.Store({
     lastInteraction: new Date(),
     resetTagsTime: 60 * seconds, //remove the filters(same as clicking all stories button)
     resetStoryTime: 15 * minutes, // close any opened story(s)
-    resetVideoTime: 20 * minutes, // Open the intro video if it isn't already
+    resetVideoTime: 2 * hours, // Open the intro video if it isn't already
     refreshPageTime: 2 * hours, // Hard refresh the page if there is some other bug
     timerLoopLength: 10 * seconds, // frequency to check if one of the above conditions has been met
   },
